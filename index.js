373a359467677cc117d4c8a7aa8f87f49a5fbe41
monisdx <135008013+monisdx@users.noreply.github.com>
2024-02-05 20:50:46 +0530

    fix coop

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ const app = express();
 dotenv.config();
 
 app.use((req, res, next) => {
-    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin; same-origin-allow-popups');
     next();
   });
   
